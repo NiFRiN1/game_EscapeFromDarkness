39d2efae41f3349796dd1b2eeda2e619c79e4ef4
ytkinroman <mc3heisenberg@gmail.com>
2024-09-12 20:20:04 +0500

    Update files

--- Assets/Scripts/Platforms/DestructiblePlatform.cs
+++ Assets/Scripts/Platforms/DestructiblePlatform.cs
@@ -3,5 +3,34 @@ using System.Collections;
 
 public class DestructiblePlatform : MonoBehaviour
 {
+    public float destructionDelay = 5.0f;
 
+    public GameObject NormalVersion;
+    //public GameObject destroyVersion;
+
+    private bool isPlayerInContact = false;
+
+    private void Start()
+    {
+        //destroyVersion.SetActive(false);
+    }
+
+    public void StartDestruction()
+    {
+        if (!isPlayerInContact) {
+            isPlayerInContact = true;
+            StartCoroutine(DestroyPlatform());
+        }
+    }
+
+    private IEnumerator DestroyPlatform()
+    {
+        //Destroy(NormalVersion, destructionDelay);
+
+        yield return new WaitForSeconds(destructionDelay);
+
+        //if (destroyVersion != null) {
+        //    destroyVersion.SetActive(true);
+        //}
+    }
 }
